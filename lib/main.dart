0689536fc070f55adfad1aad3138d6c54414f812
gaya <limanegaya@gmail.com>
2024-01-25 21:29:20 +0100

    testing with messaging and images in database

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:pocketbase/pocketbase.dart';
+import 'package:http/http.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,12 +13,13 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(
         seedColor: Colors.yellow,
         brightness: Brightness.dark,
       )),
-      home: App(),
+      home: const App(),
     );
   }
 }
@@ -29,6 +32,7 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  final pb = PocketBase('http://127.0.0.1:8090');
   List<RecordModel> messages = [];
   @override
   void initState() {
@@ -37,11 +41,9 @@ class _AppState extends State<App> {
   }
 
   void pocketBase() async {
-    final pb = PocketBase('http://127.0.0.1:8090');
-    final userData = await pb
+    await pb
         .collection('users')
         .authWithPassword('limanegaya@gmail.com', '123456789');
-    print('User is: ${userData.record}');
     final result = await pb.collection('posts').getList(
           page: 1,
           perPage: 20,
@@ -54,23 +56,70 @@ class _AppState extends State<App> {
     }
 
     pb.collection('posts').subscribe("*", (e) {
-      print('Update: ${e.action}');
-      print('Update: ${e.record}');
-      if (e.record != null && e.action == 'create') messages.add(e.record!);
+      if (e.record != null && e.action == 'create') {
+        messages.insert(0, e.record!);
+      }
       if (mounted) {
         setState(() {});
       }
     });
   }
 
+  RecordModel? link;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: messages.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(messages[index].data['message']),
-        ),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () async {
+                final file = await ImagePicker().pickImage(
+                  source: ImageSource.gallery,
+                );
+                final file2 = await file!.readAsBytes();
+                await pb.collection('posts').create(
+                  body: {
+                    'message': 'Hello there',
+                    'sender': 'yb80zoi843a1nkf',
+                    'reseiver': '8yi8b6cn6velm43',
+                  },
+                  files: [
+                    MultipartFile.fromBytes(
+                      'file',
+                      file2,
+                      filename: 'image.jpg',
+                    ),
+                  ],
+                );
+              },
+              icon: const Icon(Icons.upload))
+        ],
+      ),
+      body: Column(
+        children: [
+          if (link != null)
+            Image.network(pb.files
+                .getUrl(
+                  link!,
+                  link!.data['file'],
+                  thumb: '50x50',
+                )
+                .toString()),
+          Expanded(
+            child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (context, index) => ListTile(
+                onTap: () {
+                  if (messages[index].data['file'] != null) {
+                    link = messages[index];
+                    setState(() {});
+                  }
+                },
+                title: Text(messages[index].data['message']),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

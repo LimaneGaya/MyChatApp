9091c6ab709d3d2f83437f95434e069d6f775f60
gaya <limanegaya@gmail.com>
2024-02-10 16:26:42 +0100

    added firebase services

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
 import 'package:feedback/feedback.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/foundation.dart' show defaultTargetPlatform;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart' show MobileAds;
+import 'package:mychatapp/firebase_options.dart';
 import 'package:mychatapp/home_screen.dart';
 import 'package:mychatapp/login_screen.dart';
 import 'package:mychatapp/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   if (defaultTargetPlatform == TargetPlatform.android) {
     MobileAds.instance.initialize();
   }
+  FirebaseAnalytics.instance;
+  FirebaseCrashlytics.instance.recordFlutterFatalError;
+  await FirebaseMessaging.instance.requestPermission(provisional: true);
+  final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+  if (apnsToken != null) print(apnsToken);
   runApp(const ProviderScope(child: BetterFeedback(child: MyApp())));
 }
 

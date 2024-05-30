767face8cd02c2bc0406d892e127c471da646683
Gaya <limanegaya@gmail.com>
2024-05-30 21:31:32 +0100

    upgraded android template and gradle

---
+++ android/app/src/main/kotlin/com/example/mychatapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gaya.mychatapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/gaya/mychatapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.gaya.mychatapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
